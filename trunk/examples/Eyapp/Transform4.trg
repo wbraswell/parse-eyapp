fefae3e3506c01d205a67f2258fe548c949742ce
casiano.rodriguez.leon <casiano.rodriguez.leon@a75ea6fe-dcac-11dd-8561-ef618b9164eb>
2009-12-16 11:03:52 +0000

    prefixes and transformation example in exaples/Eyapp updated

--- trunk/examples/Eyapp/Transform4.trg
+++ trunk/examples/Eyapp/Transform4.trg
@@ -10,3 +10,15 @@ whatever_times_zero: TIMES(., ., NUM($x)) and { $x->{attr} == 0 } => { $_[0] = $
 /* rules related with times */
 times_zero = zero_times_whatever whatever_times_zero;
 
+{
+=head1 SYNOPSIS
+
+Compile it with 
+
+         eyapp -m 'Calc' Rule9.yp 
+         treereg -o T.pm -p 'R::' -m T Transform4
+
+to execute, run C<foldand0rule9_4.pl> 
+
+=cut
+}
--- trunk/examples/Eyapp/foldand0rule9_4.pl
+++ trunk/examples/Eyapp/foldand0rule9_4.pl
@@ -1,15 +1,39 @@
-#!/usr/bin/perl -w
-# File: foldand0rule9_4.pl. Compile it with 
-#          eyapp -m 'Calc' Rule9.yp; treereg -o T.pm -p 'R::' -m T Transform4
+#!/usr/bin/env perl 
+=head1 SYNOPSIS
+
+   foldand0rule9_4.pl
+   
+Try inputs: 
+
+   a = 2*3+4   # Reducido a: a = 6
+   a = 2*[3+b] # syntax error
+   a=2*3*b     # Reducido a: a = 6*b
+
+Compile it with 
+
+         eyapp -m 'Calc' Rule9.yp 
+         treereg -o T.pm -p 'R::' -m T Transform4
+
+=cut
+
+use warnings;
 use strict;
 use Calc;
 use T;
 
 sub R::TERMINAL::info { $_[0]{attr} }
+
 my $parser = new Calc(yyprefix => "R::");
-my $t = $parser->YYParse( yylex => \&Calc::Lexer, yyerror => \&Calc::Error);
-print "\n***** Before ******\n";
-print $t->str."\n";
-$t->s(@T::all);
-print "\n***** After ******\n";
-print $t->str."\n";
+                   # stdin, prompt              , read one line at time
+$parser->slurp_file('','Arithmetic expression: ', "\n");
+
+my $t = $parser->YYParse;
+
+unless ($parser->YYNberr) {
+  print "\n***** Tree before the transformations ******\n";
+  print $t->str."\n";
+
+  $t->s(@T::all);
+  print "\n***** Tree after the transformations were applied ******\n";
+  print $t->str."\n";
+}
