15fbd16899d6024ea8ad9116a9f1bc44633001b1
casiano.rodriguez.leon <casiano.rodriguez.leon@a75ea6fe-dcac-11dd-8561-ef618b9164eb>
2009-12-16 11:22:56 +0000

    updating Eyapp/Rule5.yp and Eyapp/useruleandshift.pl

--- examples/Eyapp/useruleandshift.pl
+++ examples/Eyapp/useruleandshift.pl
@@ -8,7 +8,10 @@ sub SHIFTLEFT::info { $_[0]{shift} }
 insert_function('TERMINAL::info', \&TERMINAL::attr);
 
 my $parser = new Rule5();
+$parser->slurp_file('','Arithmetic expression: ', "\n");
 my $t = $parser->Run;
-print "***********\n",$t->str,"\n";
-$t->s(@Shift::all);
-print "***********\n",$t->str,"\n";
+unless ($parser->YYNberr) {
+  print "***********\n",$t->str,"\n";
+  $t->s(@Shift::all);
+  print "***********\n",$t->str,"\n";
+}
