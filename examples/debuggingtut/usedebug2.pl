6e1317277f5244d4ba39100061dc0c401f37b19b
casiano.rodriguez.leon <casiano.rodriguez.leon@a75ea6fe-dcac-11dd-8561-ef618b9164eb>
2009-12-20 22:31:05 +0000

    m

--- examples/debuggingtut/usedebug2.pl
+++ examples/debuggingtut/usedebug2.pl
@@ -1,26 +1,7 @@
 #!/usr/bin/perl -w
-# usetreebypass.pl prueba2.exp
 use strict;
 use Debug2;
 
-sub slurp_file {
-  my $fn = shift;
-  my $f;
-
-  local $/ = undef;
-  if (defined($fn)) {
-    open $f, $fn  or die "Can't find file $fn!\n";
-  }
-  else {
-    $f = \*STDIN;
-  }
-  my $input = <$f>;
-  return $input;
-}
-
-my $input = slurp_file( shift() );
-
-my $parser = Debug2->new();
-
-$parser->Run($input);
-
+my $prompt = 'Try first "D;S" and then "D; D;  S" '.
+             '(press <CR><CTRL-D> to finish): ';
+Debug2->main($prompt);
