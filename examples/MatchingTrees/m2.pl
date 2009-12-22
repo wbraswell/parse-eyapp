25abe3247d2325a110d0c9332a834650902d57c5
casiano.rodriguez.leon <casiano.rodriguez.leon@a75ea6fe-dcac-11dd-8561-ef618b9164eb>
2009-12-22 17:48:05 +0000

    m

--- examples/MatchingTrees/m2.pl
+++ examples/MatchingTrees/m2.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl -w
+#!/usr/bin/env perl 
 use strict;
 use Rule6;
 use Parse::Eyapp::Treeregexp;
@@ -10,11 +10,31 @@ Parse::Eyapp::Treeregexp->new( STRING => q{
 })->generate();
 
 # Syntax analysis
-my $parser = new Rule6();
+my $parser = Rule6->new();
 my $input = "0*0*0";
-my $t = $parser->Run(\$input);
+$parser->input(\$input);
+my $t = $parser->YYParse();
+
 print "Tree:",$t->str,"\n";
 
 # Search
 my $m = $t->m(our ($fold, $zxw, $wxz));
 print "Match Node:\n",$m->str,"\n";
+
+=head1 SYNOPSIS
+
+This example illustrates the use of the C<m> method of
+C<Parse::Eyapp::Node> objects.
+
+Compile C<Rule6.yp> first:
+
+             eyapp Rule6
+
+Run it like this:
+
+  $ ./m2.pl 
+  Tree:TIMES(TIMES(NUM(TERMINAL),NUM(TERMINAL)),NUM(TERMINAL))
+  Match Node:
+  Match[[TIMES:0:wxz]](Match[[TIMES:1:fold,zxw,wxz]])
+
+See C<perldoc> L<Parse::Eyapp::treematchingtut> for the details
