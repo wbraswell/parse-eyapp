6a79c6928c1fc6d479b22f9f714b1e257de33291
casiano.rodriguez.leon <casiano.rodriguez.leon@a75ea6fe-dcac-11dd-8561-ef618b9164eb>
2009-12-28 13:33:09 +0000

    improving reccycling example

--- trunk/examples/recycle/rewritepostfixwithactions.pl
+++ trunk/examples/recycle/rewritepostfixwithactions.pl
@@ -10,9 +10,7 @@ my $x = <STDIN>;
 # First, translate to postfix ...
 $pparser->Run($debug, $x);
 
-# Restore input for the parser (the lexical
-# analyzer was a destructive one)
-$pparser->input(\$x);
+exit(1) if $pparser->YYNberr;
 
 # And then selectively substitute 
 # some semantic actions
@@ -20,12 +18,26 @@ $pparser->input(\$x);
 
 my %s;
 $pparser->YYSetaction(
-  ASSIGN => sub { $s{$_[1]} = $_[3] },
-  PLUS   => sub { $_[1] + $_[3] },
-  TIMES  => sub { $_[1] * $_[3] },
-  DIV    => sub { $_[1] / $_[3] },
-  NEG    => sub { -$_[2] },
+  'OP:ASSIGN'   => sub { $s{$_[1]} = $_[3] },
+  'OP:PLUS'     => sub { $_[1] + $_[3] },
+  'OP:TIMES'    => sub { $_[1] * $_[3] },
+  'OP:DIV'      => sub { $_[1] / $_[3] },
+  'OP:NEG'      => sub { -$_[2] },
 );
 
-$pparser->Run($debug);
+$pparser->Run($debug, $x);
+
+$pparser->YYSetaction(
+  'EXP'           => sub { $_[1] },
+  'OPERAND:NUM'   => \&Parse::Eyapp::Driver::YYBuildAST,
+  'OPERAND:VAR'   => \&Parse::Eyapp::Driver::YYBuildAST,
+  'OP:ASSIGN'     => \&Parse::Eyapp::Driver::YYBuildAST,
+  'OP:PLUS'       => \&Parse::Eyapp::Driver::YYBuildAST,
+  'OP:TIMES'      => \&Parse::Eyapp::Driver::YYBuildAST,
+  'OP:DIV'        => \&Parse::Eyapp::Driver::YYBuildAST,
+  'OP:NEG'        => \&Parse::Eyapp::Driver::YYBuildAST,
+);
+
+my $t = $pparser->Run($debug, $x);
+print $t->str."\n";
 
