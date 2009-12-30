b6585a57fb47c30a5e15111c48eb6ba38c699150
casiano.rodriguez.leon <casiano.rodriguez.leon@a75ea6fe-dcac-11dd-8561-ef618b9164eb>
2009-12-30 14:35:33 +0000

    eyapp to groovy via YML

--- examples/yaml/hello.pl
+++ examples/yaml/hello.pl
@@ -1,6 +1,10 @@
 #!/usr/bin/env perl -w
 use strict;
 
+# The only purpose of this file is to check a 'bug' in Groovy
+# arguments aren't properly parsed in 'string.execute()'
+# See program: thegroovyexecuteproblem.groovy
+
 print "Hello world:\n";
 my $i = 0;
 for (@ARGV) {
