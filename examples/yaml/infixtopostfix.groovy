5ef307bf7f5d5891b5b726d9239ef8acacee1d7d
casiano.rodriguez.leon <casiano.rodriguez.leon@a75ea6fe-dcac-11dd-8561-ef618b9164eb>
2009-12-30 08:47:04 +0000

    Groovy seems to protect with quotes the strings in the .execute() method. Mmmm!

--- examples/yaml/infixtopostfix.groovy
+++ examples/yaml/infixtopostfix.groovy
@@ -1,5 +1,7 @@
 #!/usr/bin/env groovy
 
-tree = '/Users/casianorodriguezleon/LEyapp/examples/yaml/Calc.pm -t -c \'a=2\''.execute().text;
+// There seems to ba a problem with quotes in the way Groovy strings are called
+//tree = "/Users/casianorodriguezleon/LEyapp/examples/yaml/Calc.pm -t -i -c a=2 2>&1".execute().text;
+tree = "/Users/casianorodriguezleon/LEyapp/examples/yaml/Calc.pm -t -i -f entrada 2>&1".execute().text;
 println "Salida: $tree"
 
